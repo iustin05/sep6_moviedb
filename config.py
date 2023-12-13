b5d84cffb7c58d1e65842227704d6637bf597c7e
Iustin G <iusgrig@icloud.com>
2023-12-13 17:55:37 +0200

    Production Config

--- config.py
+++ config.py
@@ -26,6 +26,14 @@ class DevelopmentConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    AZURE_DATABASE_URI = os.environ.get('AZURE_DATABASE_URI')
+    params = urllib.parse.quote_plus("Driver={ODBC Driver 18 for SQL Server};"
+                                     "Server=tcp:sep6moviedb.database.windows.net,1433;"
+                                     "Database=sep6_movie;"
+                                     "Uid=sep6user;"
+                                     "Pwd=rYvvuh-7gomki-tyxhot;"
+                                     "Encrypt=yes;"
+                                     "TrustServerCertificate=no;"
+                                     "Connection Timeout=30;")
+
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
     
